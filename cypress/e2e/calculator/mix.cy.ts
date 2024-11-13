bb83bfa5f91a6c2666a312cbd03fba2b7ea1308c
vectr444 <juan_c_soares@estudante.sesisenai.org.br>
2024-11-12 21:27:46 -0300

    Descomentado código para gerar erro e testar o github actions

--- cypress/e2e/calculator/mix.cy.ts
+++ cypress/e2e/calculator/mix.cy.ts
@@ -63,14 +63,14 @@ describe("Calculos Combinados - Operações Mistas", () => {
     cy.get('[data-cy="display-container"]').should("have.text", "40");
   });
 
-  // //Erro
-  // it("1 + 2 * 3", () => {
-  //   cy.get('[data-cy="1"]').click();
-  //   cy.get('[data-cy="+"]').click();
-  //   cy.get('[data-cy="2"]').click();
-  //   cy.get('[data-cy="x"]').click();
-  //   cy.get('[data-cy="3"]').click();
-  //   cy.get('[data-cy="="]').click();
-  //   cy.get('[data-cy="display-container"]').should("have.text", "7");
-  // });
+  //Erro
+  it("1 + 2 * 3", () => {
+    cy.get('[data-cy="1"]').click();
+    cy.get('[data-cy="+"]').click();
+    cy.get('[data-cy="2"]').click();
+    cy.get('[data-cy="x"]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy="display-container"]').should("have.text", "7");
+  });
 });
